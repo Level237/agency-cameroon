ec3f0d5ef0b7f35277a53e738637ac09849fd165
level237 <Bramslevel129@gmail.com>
2024-03-25 16:38:45 +0100

    update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
     NavigationMenu,
     NavigationMenuContent,
@@ -7,6 +8,8 @@ import {
     NavigationMenuTrigger,
     navigationMenuTriggerStyle,
   } from "@/components/ui/navigation-menu"
+import Link from "next/link"
+import { FaSearch } from "react-icons/fa";
 
 export default function Header() {
   return (
@@ -15,9 +18,60 @@ export default function Header() {
         <div className="flex-1">
         <h2 className="text-white text-3xl font-bold">Agency</h2>
         </div>
-        <div className="text-white">
-            le
+        <div className="w-[400px]">
+        <NavigationMenu className="text-white">
+            <NavigationMenuList>
+            <NavigationMenuItem className="">
+            <NavigationMenuTrigger>Services</NavigationMenuTrigger>
+            <NavigationMenuContent className="bg-[#09090B] text-white">
+                <ul className="">
+                <li className="row-span-3 ">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    <Link href="/" className="">Developpement web</Link>
+                </NavigationMenuLink>
+                </li>
+                </ul>
+            </NavigationMenuContent>
             
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+            <NavigationMenuTrigger>PortFolio</NavigationMenuTrigger>
+            <NavigationMenuContent>
+                <ul className="p-4">
+                <li className="row-span-3">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    <Link href="/" className="text-white">Developpement web</Link>
+                    
+                </NavigationMenuLink>
+                </li>
+                <li className="row-span-3">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    <Link href="/" className="text-white">Branding</Link>
+                    
+                </NavigationMenuLink>
+                </li>
+                </ul>
+            </NavigationMenuContent>
+            
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+            
+            <NavigationMenuLink>
+                    <Link href="/" className="text-white">About us</Link>
+                </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    <Link href="/" className="">Contact</Link>
+                </NavigationMenuLink>
+            
+            
+            </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+        </div>
+        <div className="w-[40px] text-white cursor-pointer">
+            <FaSearch/>
         </div>
         
     </div>
