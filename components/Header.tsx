86d03cd6a4edba9e05573f669e6a248965bbfea3
level237 <Bramslevel129@gmail.com>
2024-03-25 19:44:30 +0100

    added variant themes

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,15 +8,23 @@ import {
     NavigationMenuTrigger,
     navigationMenuTriggerStyle,
   } from "@/components/ui/navigation-menu"
+import Image from "next/image";
+
 import Link from "next/link"
 import { FaSearch } from "react-icons/fa";
 
 export default function Header() {
   return (
-   <header className="w-[100%] border-b-[0.1px] border-[#968d8d60] sticky top-0 z-50  py-4 px-4 sm:px-10  font-[sans-serif] min-h-[70px]">
+   <header className="w-[100%]  border-b-[0.1px] border-[#e09a3925] sticky top-0 z-50  py-4 px-4 sm:px-10 bg-[#09090B]  font-[sans-serif] min-h-[70px]">
     <div className="flex flex-wrap items-center gap-x-2 max-lg:gap-y-6 justify-between">
         <div className="flex-1">
-        <h2 className="text-white text-3xl font-bold">Agency</h2>
+            <Image 
+            src="/agency.png"
+            width="70"
+            height="70"
+            alt="logo agency cameroon"
+            />
+       
         </div>
         <div className="w-[400px]">
         <NavigationMenu className="text-white">
@@ -36,7 +44,7 @@ export default function Header() {
             </NavigationMenuItem>
             <NavigationMenuItem>
             <NavigationMenuTrigger>PortFolio</NavigationMenuTrigger>
-            <NavigationMenuContent>
+            <NavigationMenuContent className="bg-[#09090B] text-white">
                 <ul className="p-4">
                 <li className="row-span-3">
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 export default function Hero() {
   return (
@@ -36,9 +37,7 @@ export default function Hero() {
       <p className="mt-6 text-lg leading-8 text-gray-300">Elevate Your Productivity With Zynx, Your Personalized Workflow
         Assistant</p>
       <div className="mt-5 flex items-center justify-center gap-x-6">
-        <a href="/register"
-          className="rounded-md bg-sky-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400"
-          rel="noreferrer">Try Now →</a>
+        <Button variant="default" className='text-black'>our portfolio</Button>
       </div>
     </div>
   </div>
