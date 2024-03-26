9bca0d526ea387f4ec4b5f9ffd8df8f07e2f661d
level237 <Bramslevel129@gmail.com>
2024-03-26 11:02:54 +0100

    component badge added

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
    <section className="relative">
    <Hero/>
     <div className="container absolute mt-[-52px]  flex justify-center items-center">
-    <div className="flex border border-[#e09a3956]  mb-36 justify-center rounded bg-[#2b0f10] w-[80vw] h-full">
+    <div className="flex border border-[#e09a3956] z-50  mb-36 justify-center rounded bg-[#2b0f10a4] w-[80vw] h-full">
       <Image
             src="/agency.png"
             width="100"
@@ -50,6 +50,10 @@ export default function Home() {
       </div>
     </div>
       
+   </section>
+
+   <section>
+    
    </section>
    </div>
   );
