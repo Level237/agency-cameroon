a2dc5b05c68e99fe36209437a0cf2d86fd795542
level237 <Bramslevel129@gmail.com>
2024-03-26 11:15:36 +0100

    segmentation ui:list brand

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Image from "next/image";
-
+import { Badge } from "@/components/ui/badge"
+import ListBrand from "@/components/List-brand";
 export default function Home() {
   return (
    <div>
@@ -10,50 +11,13 @@ export default function Home() {
    <section className="relative">
    <Hero/>
     <div className="container absolute mt-[-52px]  flex justify-center items-center">
-    <div className="flex border border-[#e09a3956] z-50  mb-36 justify-center rounded bg-[#2b0f10a4] w-[80vw] h-full">
-      <Image
-            src="/agency.png"
-            width="100"
-            height="100"
-            alt="logo agency cameroon"
-            />
-            <Image
-            src="/agency.png"
-            width="100"
-            height="100"
-            alt="logo agency cameroon"
-            />
-            <Image
-            src="/agency.png"
-            width="100"
-            height="100"
-            alt="logo agency cameroon"
-            />
-            <Image
-            src="/agency.png"
-            width="100"
-            height="100"
-            alt="logo agency cameroon"
-            />
-             <Image
-            src="/agency.png"
-            width="100"
-            height="100"
-            alt="logo agency cameroon"
-            />
-             <Image
-            src="/agency.png"
-            width="100"
-            height="100"
-            alt="logo agency cameroon"
-            />
-      </div>
+      <ListBrand/>
     </div>
       
    </section>
 
    <section>
-    
+
    </section>
    </div>
   );
---
+++ components/List-brand.tsx
@@ -0,0 +1,47 @@
+'use client'
+import Image from 'next/image'
+import React from 'react'
+
+export default function ListBrand() {
+  return (
+    <div className="flex border border-[#e09a3956] z-50  mb-36 justify-center rounded bg-[#2b0f10a4] w-[80vw] h-full">
+    
+      <Image
+            src="/agency.png"
+            width="100"
+            height="100"
+            alt="logo agency cameroon"
+            />
+            <Image
+            src="/agency.png"
+            width="100"
+            height="100"
+            alt="logo agency cameroon"
+            />
+            <Image
+            src="/agency.png"
+            width="100"
+            height="100"
+            alt="logo agency cameroon"
+            />
+            <Image
+            src="/agency.png"
+            width="100"
+            height="100"
+            alt="logo agency cameroon"
+            />
+             <Image
+            src="/agency.png"
+            width="100"
+            height="100"
+            alt="logo agency cameroon"
+            />
+             <Image
+            src="/agency.png"
+            width="100"
+            height="100"
+            alt="logo agency cameroon"
+            />
+      </div>
+  )
+}
