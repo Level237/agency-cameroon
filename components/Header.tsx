c7f622f92dd85538cd87b9718c51bada31695f06
level237 <Bramslevel129@gmail.com>
2024-03-26 18:00:34 +0100

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,13 +20,13 @@ import Search from "./Search";
 
 export default function Header() {
   return (
-   <header className="w-[100%]  z-[300]   border-b-[0.1px] border-[#e09a3925] fixed top-0  py-4 px-4 sm:px-10 bg-[#09090B]  font-[sans-serif] min-h-[70px]">
+   <header className="w-[100%]  z-[300]   border-b-[0.1px] border-[#e09a3925] fixed top-0  py-4 px-4 sm:px-10 bg-[#09090B]  font-[sans-serif] min-h-[60px]">
     <div className="flex flex-wrap items-center gap-x-2 max-lg:gap-y-6 justify-between">
         <div className="flex-1">
             <Image 
             src="/agency.png"
-            width="70"
-            height="70"
+            width="40"
+            height="40"
             alt="logo agency cameroon"
             />
        
--- components/services/List-services.tsx
+++ components/services/List-services.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+import {motion} from "framer-motion"
 import {
     Card,
     CardContent,
@@ -8,46 +8,87 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
+import Image from "next/image"
+import { Badge } from "../ui/badge"
   
 export default function ListServices() {
   return (
-    <section className="grid grid-cols-3 mb-5 mt-20 gap-5">
- <Card>
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
-  </CardFooter>
-</Card>
-<Card>
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
-  </CardFooter>
-</Card>
-<Card>
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
-  </CardFooter>
-</Card>
+    <section className="grid grid-cols-3 mb-5  gap-5 max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
+      <motion.div
+     
+        className="relative"
+      >
+      <Card className="w-full border-[#e09a3925]">
+      <Image
+        alt="graphic design"
+
+        className="aspect-video overflow-hidden rounded-t-lg object-cover"
+        height={225}
+        src="/graphic.jpg"
+        width={400}
+      />
+      <CardContent className="rounded-b-lg p-6">
+        <CardTitle className=" flex justify-between items-center">
+          <h3 className="text-lg text-[#e09a39]">
+          Graphic Design
+          </h3>
+          <div>
+<Badge variant="outline" className="text-white border-[#cc3333] font-thin">creative</Badge>
+          </div>
+          </CardTitle>
+      </CardContent>
+      <CardFooter className="flex justify-center">
+            
+      </CardFooter>
+    </Card>
+      </motion.div>
+ 
+    <Card className="w-full border-[#e09a3925]">
+      <Image
+        alt="Cover image"
+
+        className="aspect-video overflow-hidden rounded-t-lg object-cover"
+        height={225}
+        src="/design.jpg"
+        width={400}
+      />
+      <CardContent className="rounded-b-lg p-6">
+      <CardTitle className=" flex justify-between items-center">
+          <h3 className="text-lg text-[#e09a39]">
+          Web Design
+          </h3>
+          <div>
+<Badge variant="outline" className="text-white border-[#cc3333] font-thin">creative</Badge>
+          </div>
+          </CardTitle>
+      </CardContent>
+      <CardFooter className="flex justify-center">
+        
+      </CardFooter>
+    </Card>
+    <Card className="w-full border-[#e09a3925]">
+      <Image
+        alt="Cover image"
+
+        className="aspect-video overflow-hidden rounded-t-lg object-cover"
+        height={225}
+        src="/web.jpg"
+        width={400}
+      />
+      <CardContent className="rounded-b-lg p-6">
+      <CardTitle className=" flex justify-between items-center">
+          <h3 className="text-lg text-[#e09a39]">
+          Graphic Design
+          </h3>
+          <div>
+<Badge variant="outline" className="text-white border-[#e09a39] font-thin">IT</Badge>
+          </div>
+          </CardTitle>
+      </CardContent>
+      <CardFooter className="flex justify-center">
+        
+      </CardFooter>
+    </Card>
     </section>
    
   )
