ad48f1b08af62438cc9cf8d5918bead584c8d3f6
level237 <Bramslevel129@gmail.com>
2024-03-28 11:32:00 +0100

    animated tab component

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import ListBrand from "@/components/List-brand";
 import ListServices from "@/components/services/List-services";
 import InfiniteCarousel from "@/components/InfiniteCarousel";
 import PortfolioAnimate from "@/components/PortfolioAnimate";
+import AnimatedTabs from "@/components/AnimatedTab";
+import { Separator } from "@/components/ui/separator";
 export default function Home() {
   return (
    <div>
@@ -25,7 +27,14 @@ export default function Home() {
     <PortfolioAnimate/>
     
       <ListServices/>
+      <section className="mt-20">
+      <h1 className="mt-10 font-primary text-2xl font-bold tracking-tight text-[#e8d1bb] sm:text-4xl">Our Realisation</h1>
+      <div className="mt-12 flex flex-col items-center justify-center">
+      <AnimatedTabs/>
+      <Separator className="w-[90%] mt-3 h-[0.2px] mb-20 bg-[#e8d1bb2a]"/>
+      </div>
       
+      </section>
    </section>
    </div>
   );
