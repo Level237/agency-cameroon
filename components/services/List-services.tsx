78c5ba42599c9dff91f1c1ecd6a0db97a3d74e1f
level237 <Bramslevel129@gmail.com>
2024-03-28 09:48:16 +0100

    List services component

--- components/services/List-services.tsx
+++ components/services/List-services.tsx
@@ -1,84 +1,64 @@
 'use client'
-import {motion} from "framer-motion"
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-import Image from "next/image"
-import { Badge } from "../ui/badge"
-  
+import { PinContainer } from '../ui/3d-pin';
+import { Separator } from "@/components/ui/separator";
 export default function ListServices() {
   return (
-    <section className="grid grid-cols-3 mb-5  gap-5 max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
-      <motion.div
-     
-        className="relative"
+   <>
+   <div className="flex flex-col justify-center items-center">
+        <h2 className="text-white font-bold text-4xl font-primary">our Services</h2>
+        <Separator className="w-36 h-1 mr-16 bg-[#e09a39]"/>
+      </div>
+      <div className="grid mt-11 mb-11 grid-cols-3 container">
+      <PinContainer
+        title="Design Graphic"
+        link="home"
       >
-      <Card className="w-full h-[380px] border-[#e09a3925]">
-      <Image
-        alt="graphic design"
-
-        className="aspect-video h-full overflow-hidden rounded-t-lg object-cover"
-        height={225}
-        src="/graphic.jpg"
-        width={400}
-      />
-     
-     
-    </Card>
-      </motion.div>
- 
-    <Card className="w-full border-[#e09a3925]">
-      <Image
-        alt="Cover image"
-
-        className="aspect-video overflow-hidden rounded-t-lg object-cover"
-        height={225}
-        src="/design.jpg"
-        width={400}
-      />
-      <CardContent className="rounded-b-lg p-6">
-      <CardTitle className=" flex justify-between items-center">
-          <h3 className="text-lg text-[#e09a39]">
-          Web Design
+        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+          <h3 className="max-w-xs !pb-2 !m-0 font-bold font-primary text-[#e8d1bb]  text-base text-slate-100">
+            Design graphic
           </h3>
-          <div>
-<Badge variant="outline" className="text-white border-[#cc3333] font-thin">creative</Badge>
+          <div className="text-base !m-0 !p-0 font-normal">
+            <span className="text-slate-500 font-secondary">
+              Customizable Tailwind CSS and Framer Motion Components.
+            </span>
           </div>
-          </CardTitle>
-      </CardContent>
-      <CardFooter className="flex justify-center">
-        
-      </CardFooter>
-    </Card>
-    <Card className="w-full border-[#e09a3925]">
-      <Image
-        alt="Cover image"
-
-        className="aspect-video overflow-hidden rounded-t-lg object-cover"
-        height={225}
-        src="/web.jpg"
-        width={400}
-      />
-      <CardContent className="rounded-b-lg p-6">
-      <CardTitle className=" flex justify-between items-center">
-          <h3 className="text-lg text-[#e09a39]">
-          Graphic Design
+          <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+        </div>
+      </PinContainer>
+      <PinContainer
+        title="web development"
+        link="home"
+      >
+        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+          <h3 className="max-w-xs !pb-2 !m-0 font-bold font-primary  text-base text-slate-100">
+            web development
           </h3>
-          <div>
-<Badge variant="outline" className="text-white border-[#e09a39] font-thin">IT</Badge>
+          <div className="text-base !m-0 !p-0 font-normal">
+            <span className="text-slate-500 font-secondary">
+              Customizable Tailwind CSS and Framer Motion Components.
+            </span>
           </div>
-          </CardTitle>
-      </CardContent>
-      <CardFooter className="flex justify-center">
-        
-      </CardFooter>
-    </Card>
-    </section>
+          <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+        </div>
+      </PinContainer>
+      <PinContainer
+        title="UI Design"
+        link="/web"
+      >
+        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+          <h3 className="max-w-xs !pb-2 !m-0 font-bold font-primary   text-base text-slate-100">
+            UI Design
+          </h3>
+          <div className="text-base !m-0 !p-0 font-normal">
+            <span className="text-slate-500 font-secondary">
+              Customizable Tailwind CSS and Framer Motion Components.
+            </span>
+          </div>
+          <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+        </div>
+      </PinContainer>
+      </div>
+   </>
    
   )
 }
